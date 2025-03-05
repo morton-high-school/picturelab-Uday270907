4a5d687299ab3bed47857a92346f63be8f082f82
Uday270907 <uday.anand@mcusd709.org>
2025-03-05 08:14:11 -0600

    Update Picture.java

--- classes/Picture.java
+++ classes/Picture.java
@@ -179,7 +179,7 @@ public class Picture extends SimplePicture
     int width = pixels[0].length;
     for (int row = 0; row < pixels.length; row++)
     {
-      for (int col = width; col > width / 2; col--)
+      for (int col = width-1; col > width / 2; col--)
       {
         leftPixel = pixels[row][col];
         rightPixel = pixels[row][width - 1 - col];
